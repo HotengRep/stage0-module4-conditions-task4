d977a8883b2750e93a2ce926514bb75f15ea08c4
Makar <hoteng789@gmail.com>
2023-07-22 14:26:13 +0400

    Ternary operators

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int gretest = first;
+        gretest  = first < second ?
+                second:
+                first;
+
+        gretest = gretest < third ?
+                third:
+                gretest;
+
+        System.out.println(gretest);
     }
 }
